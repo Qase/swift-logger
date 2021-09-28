b0f0120717580582f82dcc7cf252cc74ba4b8549
Dagy Tran <anh.duc.tran@etnetera.eu>
2021-09-28 15:36:56 +0200

    System logger refactoring

--- Sources/Logger/Loggers/SystemLogger.swift
+++ Sources/Logger/Loggers/SystemLogger.swift
@@ -11,41 +11,33 @@ import os
 /// Pre-built logger that wraps system os_logger
 public class SystemLogger: Logging {
 
-    private var logger: OSLog?
+    private var logger: OSLog
 
     public init(subsystem: String, category: String) {
-        if #available(iOS 10, *) {
-            logger = OSLog(subsystem: subsystem, category: category)
-        }
-    }
-
-    private func systemLevel(forLevel level: Level) -> OSLogType {
-        if #available(iOS 10, *) {
-            switch level {
-            case .info:
-                return .info
-            case .debug:
-                return .debug
-            case .verbose, .warn, .error:
-                return .default
-            case .system:
-                return .fault
-            case .process:
-                return .error
-            }
-        } else {
-            return .default
-        }
+        logger = OSLog(subsystem: subsystem, category: category)
     }
 
     public var levels: [Level] = [.info]
 
     public func log(_ message: String, onLevel level: Level) {
-        guard let _logger = logger else { return }
+        let staticMessage = "\(messageHeader(forLevel: level)) \(message)"
+        os_log("%@", log: logger, type: level.logType, staticMessage)
+    }
+}
 
-        if #available(iOS 10, *) {
-            let staticMessage = "\(messageHeader(forLevel: level)) \(message)"
-            os_log("%@", log: _logger, type: systemLevel(forLevel: level), staticMessage)
+private extension Level {
+    var logType: OSLogType {
+        switch self {
+        case .info:
+            return .info
+        case .debug:
+            return .debug
+        case .verbose, .warn, .error:
+            return .default
+        case .system:
+            return .fault
+        case .process:
+            return .error
         }
     }
 }
