7799eca060bd4ff8d48d3f701a9a45ece668c77e
Martin Troup <martin.troup@etnetera.eu>
2022-02-22 16:13:31 +0100

    getArchivedLogFilesUrl available via LoggerManager. The public functions commented.

--- Sources/Logger/LoggerManager.swift
+++ Sources/Logger/LoggerManager.swift
@@ -22,10 +22,18 @@ public class LoggerManager {
 
     private var subscriptions = Set<AnyCancellable>()
 
-    public func logger(withID id: UUID) -> Logging? {
+    /// Get a specific registered logger from the manager.
+    /// - Parameter id: unique identifier of the logger.
+    public func logger(withId id: UUID) -> Logging? {
       loggers.first { $0.id == id }
     }
-    
+
+    /// `LoggerManager` initialization
+    /// - Parameters:
+    ///   - loggingConcurrencyMode: concurrency mode in which the manager logs via its registered loggers.
+    ///   - loggers: registered loggers.
+    ///   - applicationCallbackLoggerBundle: application lifecycle callbacks that are to be logged by the manager.
+    ///   - metaInformationLoggerBundle: meta information to be logged by the manager.
     public init(
         loggingConcurrencyMode: LoggingConcurrencyMode = .asyncSerial(.defaultSerialLoggingQueue),
         loggers: [Logging],
@@ -56,10 +64,13 @@ public class LoggerManager {
         }
     }
 
-    /// All logs from FileLogger files (represented as `Data`).
-    public func perFileLogDataIfAvailable(forLoggerWithID loggerID: UUID? = nil) -> [URL: Data]? {
+
+    /// All logged data from `FileLogger` in a form of `[URL: Data]`. `URL` represents a specific log file while `Data` is the content of the file.
+    /// NOTE: This function returns any data only if at least one `FileLogger` is registed within the manager.
+    /// - Parameter loggerId: ID of a specific `FileLogger` instance if more are registed. If not specified, all files from all `FileLogger` instances are to be returned.
+    public func perFileLogDataIfAvailable(forLoggerWithId loggerId: UUID? = nil) -> [URL: Data]? {
         loggingConcurrencyMode.serialQueue.sync {
-            (loggerID.flatMap(logger(withID:)).map { [$0] } ?? loggers)
+            (loggerId.flatMap(logger(withId:)).map { [$0] } ?? loggers)
                 .compactMap { $0 as? FileLogger }
                 .compactMap { $0.perFileLogData }
                 .flatMap { $0 }
@@ -72,25 +83,55 @@ public class LoggerManager {
         }
     }
 
+    /// All logged data from `FileLogger` in a form of `FileLogEntry` array. `FileLogEntry` represents a single well structured log entry from a `FileLogger`.
+    /// NOTE: This function returns any data only if at least one `FileLogger` is registed within the manager.
+    /// - Parameters:
+    ///   - loggerId: ID of a specific `FileLogger` instance if more are registed. If not specified, all log entries from all `FileLogger` instances are to be returned.
+    ///   - filter: Additional filter can be applied to specify what entries are to be returned.
     public func logFilesRecords(
-      forLoggerWithID loggerID: UUID? = nil,
+      forLoggerWithId loggerId: UUID? = nil,
       filteredBy filter: (FileLogEntry) -> Bool = { _ in true }
     ) -> [FileLogEntry]? {
-        (loggerID.flatMap(logger(withID:)).map { [$0] } ?? loggers)
+        (loggerId.flatMap(logger(withId:)).map { [$0] } ?? loggers)
             .compactMap { $0 as? FileLogger }
             .compactMap { $0.logFilesRecords(filteredBy: filter) }
             .flatMap { $0 }
     }
 
-    /// Method to delete all log files if there are any.
-    public func deleteAllLogFilesIfAvailable(forLoggerWithID loggerID: UUID? = nil) {
+
+    /// Delete all log files created by an instance of `FileLogger`.
+    /// NOTE: This function returns any data only if at least one `FileLogger` is registed within the manager.
+    /// - Parameters:
+    ///   - loggerId: ID of a specific `FileLogger` instance if more are registed. If not specified, all files from all `FileLogger` instances are to be deleted.
+    public func deleteAllLogFilesIfAvailable(forLoggerWithId loggerId: UUID? = nil) {
         loggingConcurrencyMode.serialQueue.async {
-            (loggerID.flatMap(self.logger(withID:)).map { [$0] } ?? self.loggers)
+            (loggerId.flatMap(self.logger(withId:)).map { [$0] } ?? self.loggers)
                 .compactMap { $0 as? FileLogger }
                 .forEach { try? $0.deleteAllLogFiles() }
         }
     }
 
+    /// Get `URL` with archived log files bundle for a single `FileLogger`.
+    /// NOTE: This function returns any data only if at least one `FileLogger` is registed within the manager.
+    /// - Parameters:
+    ///   - loggerId: ID of a specific `FileLogger` instance if more are registed. If not specified, the archive for the first found `FileLogger` instance will be returned.
+    ///   - fileName: Name of the archived file.
+    public func getArchivedLogFilesUrl(
+        forLoggerWithId loggerId: UUID? = nil,
+        withFileName fileName: String? = nil
+    ) -> URL? {
+        let fileLoggerById = loggerId.flatMap(self.logger(withId:)).flatMap { $0 as? FileLogger }
+
+        return (fileLoggerById ?? self.loggers.compactMap { $0 as? FileLogger }.first)?
+            .getArchivedLogFilesUrl(withFileName: fileName)
+    }
+
+    /// Log specific entity via the manager.
+    /// - Parameters:
+    ///   - message: Entity to be logged.
+    ///   - file: Name of the file where the logging occures.
+    ///   - function: Name of the function where the logging occures.
+    ///   - line: Number of the line where the logging occures.
     public func log(
         _ message: CustomStringConvertible,
         onLevel level: Level,
